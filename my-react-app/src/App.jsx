3f1a6d712e79a9d238b2751f55b6ef85a317ee12
Nathan Hor <nathanhor2001@gmail.com>
2025-12-07 05:29:35 +0000

    made changes to navbar, when user loads up page it shows the home page immediately

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home.jsx';
 import SearchAnim from './pages/SearchAnim.jsx';
@@ -10,12 +10,12 @@ function App() {
     <BrowserRouter>
       <nav className ="navbar">
         <div className = "nav-links">
-          <Link to ="/home">Home</Link>
+          <Link to ="/">Home</Link>
           <Link to ="/searchAnim">SearchAnim</Link>
           </div>
       </nav>
       <Routes>
-        <Route path="/home" element={ <Home /> } />
+        <Route path="/" element={ <Home /> } />
         <Route path="/searchAnim" element={ <SearchAnim /> } />
       </Routes>
     </BrowserRouter>
