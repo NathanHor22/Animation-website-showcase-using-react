ae77964fa4b73710f3897ad2ecdc83126b7ee32d
Nathan Hor <nathanhor2001@gmail.com>
2025-12-08 04:20:39 +0000

    search bar fixed for frontend connecting it to the animation data is the next ticket

--- my-react-app/src/pages/SearchAnim.jsx
+++ my-react-app/src/pages/SearchAnim.jsx
@@ -1,7 +1,13 @@
+import './SearchAnim.css';
+import Animation from '../components/animation';
+
 function SearchAnim() {
     return (
         <>
-        
+            <h2>Look at our selection of animation</h2>
+            <input type ="text"className = "search-bar" placeholder="Search for animations...">
+
+            </input>
         </>
     )
 }
